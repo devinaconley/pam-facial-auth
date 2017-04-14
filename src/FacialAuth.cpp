5a80f2526d0fe4fa5819ce23c401ebd6d45be12e
Devin Conley <devinconley@jhu.edu>
2017-04-14 05:19:15 -0400

    proper cropping and resizing for input images

--- src/FacialAuth.cpp
+++ src/FacialAuth.cpp
@@ -41,14 +41,20 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t * pamh, int flags, int argc, co
 
 	// Get config
 	std::map<std::string, std::string> config;
-	config["imageDir"]                   = "/var/lib/motioneye/Camera1"; // defaults
-	config["imageWindow"]                = "5";
+	config["imageDir"]    = "/var/lib/motioneye/Camera1"; // defaults
+	config["imageWindow"] = "5";
 
 	Utils::GetConfig( "/etc/pam-facial-auth/config", config );
 
+	// Parse number values
+	size_t height      = std::stoi( config["imageHeight"] );
+	size_t width       = std::stoi( config["imageWidth"] );
+	time_t timeout     = std::stoi( config["timeout"] );
+	size_t imageWindow = std::stoi( config["imageWindow"] );
+
+
 	// Find recent images of interest
 	std::vector<std::string> imagePaths;
-	size_t                   imageWindow = std::stoi( config["imageWindow"] );
 
 	// Assume daily folders named under yyyy-MM-dd
 	std::vector<std::string> dates;
@@ -78,9 +84,37 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t * pamh, int flags, int argc, co
 
 	for ( std::vector<std::string>::iterator it = imagePaths.begin(); it != imagePaths.end(); ++it )
 	{
-		cv::Mat im         = cv::imread( *it, CV_LOAD_IMAGE_GRAYSCALE );
-		double  confidence = 0.0;
-		int     prediction = -1;
+		cv::Mat im      = cv::imread( *it, CV_LOAD_IMAGE_GRAYSCALE );
+
+		// Resize properly
+		cv::Rect roi;
+		double   ratioH = double( im.rows ) / double( height );
+		double   ratioW = double( im.cols ) / double( width );
+		// First crop to get to the same aspect ratio (crop in the larger relative dimension)
+		if ( ratioH < ratioW )
+		{
+			roi.height = im.rows;
+			roi.width  = double( im.rows ) * ( double( width ) / double( height ) );
+			roi.y      = 0.0;
+			roi.x      = double( im.cols - roi.width ) / 2.0;
+		}
+		else
+		{
+			roi.width  = im.cols;
+			roi.height = double( im.cols ) * ( double( height ) / double( width ) );
+			roi.x      = 0.0;
+			roi.y      = double( im.rows - roi.height ) / 2.0;
+		}
+		// Then do crop and resize
+		cv::Size sz;
+		sz.height = height;
+		sz.width  = width;
+		im = im( roi );
+		cv::resize( im, im, sz );
+
+		// Do prediction
+		double confidence = 0.0;
+		int    prediction = -1;
 		fr->predict( im, prediction, confidence );
 
 		printf( "Predicted: %s, %d, %s (%f)\n",
